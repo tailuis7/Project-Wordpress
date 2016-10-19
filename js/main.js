54e04ff72c0b46f402eb195a33343b7b3176152b
Tai Luis <tailuis7@gmail.com>
2016-10-19 14:26:58 +0700

    Almost done Menu and Header.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-    
+    $('.slider').owlCarousel({
+        items: 1,
+        center: true,
+        loop: true,
+        margin: 0,
+        dots: false,
+        autoplay: true,
+        autoplayHoverPause: true,
+    });
 });
