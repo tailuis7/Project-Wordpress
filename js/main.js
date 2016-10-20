273574e2a3db8d00f3bc7d3b382b6d9e30630e8d
Tai Luis <tailuis7@gmail.com>
2016-10-20 17:32:39 +0700

    Almost done. Just Fixtures, Table, Footer, Rightbar left.

--- js/main.js
+++ js/main.js
@@ -8,4 +8,27 @@ $(document).ready(function() {
         autoplay: true,
         autoplayHoverPause: true,
     });
+
+    $('.slider-2').owlCarousel({
+        items: 5,
+        loop: true,
+        margin: 10,
+        dots: true,
+        autoplay: true,
+        dotsContainer: '#carousel-custom-dots',
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+    $('.owl-dot').click(function() {
+        $('.slider-2').trigger('to.owl.carousel', [$(this).index(), 100]);
+    });
 });
