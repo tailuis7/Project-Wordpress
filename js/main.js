dea01f46b3a170397633993746ffe550032d7491
Tai Luis <tailuis7@gmail.com>
2016-10-21 17:00:58 +0700

    Pretty good! Just fixtures left.

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
             }
         }
     });
-    $('.owl-dot').click(function() {
+    $('#carousel-custom-dots .owl-dot').click(function() {
         $('.slider-2').trigger('to.owl.carousel', [$(this).index(), 100]);
     });
 
@@ -44,9 +44,33 @@ $(document).ready(function() {
         navText: ['<a class="prev">&lt;</a>', '<a class="next">&gt;</a>']
     });
 
+    $('.slider-4').owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 10,
+        dots: true,
+        autoplay: true,
+        dotsContainer: '#carousel-custom-dots-2',
+
+    });
+    $('#carousel-custom-dots-2 .owl-dot').click(function() {
+        $('.slider-4').trigger('to.owl.carousel', [$(this).index(), 100]);
+    });
+
     $('.go-top').click(function() {
         $('body,html').animate({
             scrollTop: 0
         }, 0);
+    });
+
+
+    $('.cmmtxt').click(function() {
+        if ($(this).children('span').css('display') == 'inline') {
+            $(this).children('span').css('display', 'none');
+            $(this).children('b').css('display', 'inline')
+        } else {
+            $(this).children('span').css('display', 'inline');
+            $(this).children('b').css('display', 'none')
+        }
     });
 });
